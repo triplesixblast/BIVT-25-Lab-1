b2ad82522138266b519b88b36c4715ee2080213b
triplesixblast <128070058+triplesixblast@users.noreply.github.com>
2025-09-08 22:21:46 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab1
+namespace Lab1
 {
     public class Blue
     {
@@ -7,7 +7,7 @@
             bool answer = false;
 
             // code here
-
+            answer = (a > 0 && b > 0) || (a < 0 && b < 0);
             // end
 
             return answer;
@@ -17,7 +17,7 @@
             bool answer = false;
 
             // code here
-
+            answer = Math.Abs(d - Math.Round(d)) >= 1e-4;
             // end
 
             return answer;
@@ -27,7 +27,10 @@
             bool answer = false;
 
             // code here
-
+            if (b != 0)
+            {
+                answer = a % b == 0;
+            }
             // end
 
             return answer;
@@ -37,7 +40,21 @@
             double answer = 0;
 
             // code here
-
+            double absd = Math.Abs(d);
+            double absf = Math.Abs(f);
+            double absg = Math.Abs(g);
+            if (absd > absf && absd > absg)
+            {
+                answer = d;
+            }
+            else if (absf > absd && absf > absg)
+            {
+                answer = f;
+            }
+            else
+            {
+                answer = g;
+            }
             // end
 
             return answer;
@@ -47,7 +64,18 @@
             double answer = 0;
 
             // code here
-
+            if (x <= -1)
+            {
+                answer = 0;
+            }
+            else if (x > -1 && x <= 0)
+            {
+                answer = x + 1;
+            }
+            else
+            {
+                answer = 1;
+            }
             // end
 
             return answer;
@@ -57,7 +85,7 @@
             bool answer = false;
 
             // code here
-
+            answer = (2 * Math.Sqrt(circleS / Math.PI)) <= Math.Sqrt(squareS);
             // end
 
             return answer;
@@ -68,7 +96,20 @@
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
             // end
 
             return answer;
@@ -78,7 +119,22 @@
             bool answer = false;
 
             // code here
-            
+            int mida = a / 2;
+            int midb = b / 2;
+            int midc = c / 2;
+            int coins = mida + midb + midc;
+            if (coins == 0)
+            {
+                answer = false;
+            }
+            else if (coins % 3 == 0 || (coins + 1) % 3 == 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
             // end
 
             return answer;
